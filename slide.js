6b6708cacc8d95beaf8f884604d608d3fa4a3567
MariaAde1 <adesinaomololamaria11@gmail.com>
2024-09-07 08:19:05 -0700

    first commit

---
+++ slide.js
@@ -0,0 +1,28 @@
+// This line declares an array called images containing paths to different images. These images will be used in the slider.
+let images = [
+    "/SlideShows/Assets/Image/web1.webp",
+    "/SlideShows/Assets/Image/web2.jpg",
+    "/SlideShows/Assets/Image/web3.jpg",
+    "/SlideShows/Assets/Image/web4.jpg",
+    "/SlideShows/Assets/Image/web2.jpg",
+    "/SlideShows/Assets/Image/web5.jpg",
+    "/SlideShows/Assets/Image/home.jfif",
+    "/SlideShows/Assets/Image/tech1.jpg"
+];
+
+// This line declares a variable currentIndex and initializes it to 0. It keeps track of the currently displayed image in the slider. The currentIndex is the index in the images array.
+let currentIndex = 0;
+
+// This line increments currentIndex by 1 to move to the next image in the array.
+function nextSlide(){
+// The % images.length part ensures that if currentIndex exceeds the length of the array (i.e., goes beyond the last image), it wraps around to 0, effectively cycling back to the first image.
+    currentIndex = (currentIndex + 1) % images.length;
+// This line updates the src attribute of an HTML element with the ID slider-image to the image at the current index in the images array, displaying the next image in the slider.
+    document.getElementById("slider-image").src = images[currentIndex];
+};
+
+// The it is decrementing currentIndex to move to the previous image.
+function prevSlide(){
+    currentIndex = (currentIndex + 1 + images.length) % images.length;
+    document.getElementById("slider-image").src = images[currentIndex];
+};
